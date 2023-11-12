8702218bc2a2553e81d9ad21e54f6998743d5379
Pavel <pavelmail07@gmail.com>
2023-11-12 19:13:26 +0000

    Added command to get date

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # kbot
 devops application from scratch
+
+
+Use this telegram bot t.me/pavelmoiseenko_bot
+due to command /start date you can get current date
--- cmd/kbot.go
+++ cmd/kbot.go
@@ -51,6 +51,9 @@ to quickly create a Cobra application.`,
 			switch payload {
 				case "hello":
 					err = m.Send(fmt.Sprintf("Hello. I'm bot %s", appVersion))
+				case "date":
+					now := time.Now()
+  					err = m.Send(fmt.Sprintf(now.Format("2006-01-02")))
 			}
 
 			return err
